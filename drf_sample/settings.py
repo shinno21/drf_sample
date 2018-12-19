1b51d61a18e236112f4d2e8e354ee6e456790efa
Shinnosuke Haba <haba.shinnosuke.mr.@mitsubishicem-sys.co.jp>
2018-12-19 14:52:27 +0900

    created test_settings to separate from settings for production use.

--- drf_sample/settings.py
+++ drf_sample/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*(d6+@a0izc%pn_-c(en&9weurmwy=yc$e5dh0pif@p$eerj5%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
---
+++ drf_sample/test_settings.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+"""テスト用のsettings
+"""
+from drf_sample.settings import *
+
+
+DEBUG = True
